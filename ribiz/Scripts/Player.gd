f1e10a7a01681a037f92ee58a59c58cd3da6bcdb
Borna Bešić <git.o8yim@aleeas.com>
2023-04-14 22:14:38 +0200

    Update Scripts/Player.gd

--- ribiz/Scripts/Player.gd
+++ ribiz/Scripts/Player.gd
@@ -8,20 +8,24 @@ func _ready():
 	pass
 
 func _process(delta):
+	var running = Input.is_key_pressed(KEY_SHIFT)
+	var speed = (running_speed if running else walking_speed)
+
 	var velocity = Vector2()
+
 	if Input.is_action_pressed("walk_right"):
-		velocity.x = (walking_speed if not Input.is_key_pressed(KEY_SHIFT) else running_speed)
+		velocity.x = speed
 		scale.x = abs(scale.x)
 	elif Input.is_action_pressed("walk_left"):
-		velocity.x = -(walking_speed if not Input.is_key_pressed(KEY_SHIFT) else running_speed)
+		velocity.x = -speed
 		scale.x = -abs(scale.x)
 
 	if Input.is_action_pressed("walk_down"):
-		velocity.y = (walking_speed if not Input.is_key_pressed(KEY_SHIFT) else running_speed)
+		velocity.y = speed
 	elif Input.is_action_pressed("walk_up"):
-		velocity.y = -(walking_speed if not Input.is_key_pressed(KEY_SHIFT) else running_speed)
+		velocity.y = -speed
 
-	if velocity.length() > 0: play("walk" if not Input.is_key_pressed(KEY_SHIFT) else "run")
+	if velocity.length_squared() > 0: play("walk" if not running else "run")
 	else: play("idle")
 
 	position += velocity * delta
