a02d59097ccc06cda4f7a6ac64dea373a7d5a962
Lovro Bosnar <lovro.bosnar1@gmail.com>
2023-04-17 13:23:37 +0200

    adding collisions to tiles. Extending player to kinematic body with collision

--- ribiz/Assets/Player/Scripts/Player.gd
+++ ribiz/Assets/Player/Scripts/Player.gd
@@ -1,13 +1,15 @@
-extends AnimatedSprite
+extends KinematicBody2D
 
 
 var walking_speed = 100;
 var running_speed = 3 * walking_speed;
+var player_animated_sprite;
 
 func _ready():
-	pass
-
-func _process(delta):
+	player_animated_sprite = get_node("Player")
+	
+func _physics_process(delta):
+	
 	var running = Input.is_key_pressed(KEY_SHIFT)
 	var speed = (running_speed if running else walking_speed)
 
@@ -15,17 +17,20 @@ func _process(delta):
 
 	if Input.is_action_pressed("walk_right"):
 		velocity.x = speed
-		scale.x = abs(scale.x)
+		player_animated_sprite.scale.x = abs(player_animated_sprite.scale.x)
 	elif Input.is_action_pressed("walk_left"):
 		velocity.x = -speed
-		scale.x = -abs(scale.x)
+		player_animated_sprite.scale.x = -abs(player_animated_sprite.scale.x)
 
 	if Input.is_action_pressed("walk_down"):
 		velocity.y = speed
 	elif Input.is_action_pressed("walk_up"):
 		velocity.y = -speed
 
-	if velocity.length_squared() > 0: play("walk" if not running else "run")
-	else: play("idle")
+	if velocity.length_squared() > 0: 
+		player_animated_sprite.play("walk" if not running else "run")
+	else: 
+		player_animated_sprite.play("idle")
 
-	position += velocity * delta
+	move_and_collide(velocity * delta)
+	
