1029988806b007a036ae128a574c505b4a60bc84
Borna Bešić <git.o8yim@aleeas.com>
2023-04-18 22:26:18 +0200

    Diminish the lamp's light with time and implement a game over HUD

--- ribiz/Assets/Player/Scripts/Player.gd
+++ ribiz/Assets/Player/Scripts/Player.gd
@@ -1,15 +1,32 @@
 extends KinematicBody2D
 
+const LAMP_RADIUS_MIN: float = 200.0
+const LAMP_ENERGY_GAME_OVER: float = 0.1
 
-var walking_speed = 100;
-var running_speed = 3 * walking_speed;
-var player_animated_sprite;
+var walking_speed: float = 100
+var running_speed: float = 3 * walking_speed
+var tree: SceneTree
+var player_animated_sprite: AnimatedSprite
+var lamp_light: Light2D
+var lamp_time_left: float = 60
+var game_over: bool = false
+var game_over_hud = preload("res://Assets/HUD/GameOver.tscn").instance()
 
 func _ready():
+	tree = get_tree()
 	player_animated_sprite = get_node("Player")
+	lamp_light = player_animated_sprite.get_node("LampSprite/LampLight")
 	
 func _physics_process(delta):
+	if not game_over:
+		update_player(delta)
+		update_lamp(delta)
+		update_game_over()
+	else:
+		yield(get_tree().create_timer(3.0), "timeout")
+		tree.reload_current_scene()
 	
+func update_player(delta):
 	var running = Input.is_key_pressed(KEY_SHIFT)
 	var speed = (running_speed if running else walking_speed)
 
@@ -31,6 +48,19 @@ func _physics_process(delta):
 		player_animated_sprite.play("walk" if not running else "run")
 	else: 
 		player_animated_sprite.play("idle")
-
+		
 	move_and_slide(velocity)
 	
+func update_lamp(delta):
+	lamp_time_left = max(lamp_time_left - delta, 0)
+	var lamp_light_scale = lamp_time_left / 60
+	var lamp_light_size = min(lamp_light.texture.get_width() * lamp_light.scale.x, lamp_light.texture.get_height() * lamp_light.scale.y)
+	if lamp_light_size > LAMP_RADIUS_MIN:
+		lamp_light.scale.x = lamp_light_scale
+		lamp_light.scale.y = lamp_light_scale
+	lamp_light.energy = lamp_time_left / 60
+
+func update_game_over():
+	if not game_over and lamp_light.energy <= LAMP_ENERGY_GAME_OVER:
+		game_over = true
+		tree.get_root().get_node("/root/Level").add_child(game_over_hud)
