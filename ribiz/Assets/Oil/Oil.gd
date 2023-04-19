dd0b6207c4693fdc14348788532f56ce1552c768
Borna Bešić <git.o8yim@aleeas.com>
2023-04-20 00:47:23 +0200

    Small refactor

--- ribiz/Assets/Oil/Oil.gd
+++ ribiz/Assets/Oil/Oil.gd
@@ -6,12 +6,11 @@ signal oil_collected(oil_amount)
 var Player = preload("res://Assets/Player/Scripts/Player.gd")
 export var amount: float = 20.0
 
-onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	add_to_group("oil")
 	animation_player.play("Bounce")
-
 
 func _on_body_entered(body):
 	if body is Player:
--- ribiz/Scenes/Level.gd
+++ ribiz/Scenes/Level.gd
@@ -1,20 +1,22 @@
 extends Node2D
 
+const GAME_OVER_TIMEOUT: float = 3.0
+
 onready var game_over_hud = preload("res://Assets/HUD/GameOver.tscn").instance()
 onready var tree = get_tree()
 var is_game_over: bool = false
 
 func _ready():
 	$Player.connect("lantern_health_changed", $LanternHealthHUD, "set_health")
-	$Player.connect("lantern_extinguished", self, "_signal_game_over")
+	$Player.connect("lantern_extinguished", self, "_set_game_over")
 	for oil in tree.get_nodes_in_group("oil"):
 		oil.connect("oil_collected", $Player, "add_lantern_oil")
 
 func _process(delta):
 	if is_game_over:
-		yield(tree.create_timer(3.0), "timeout")
+		yield(tree.create_timer(GAME_OVER_TIMEOUT), "timeout")
 		tree.reload_current_scene()
 
-func _signal_game_over():
+func _set_game_over():
 	self.add_child(game_over_hud)
 	is_game_over = true
