623e6baad2f2c652fed863a85441511a74f7d042
Borna Bešić <bornabesic@users.noreply.github.com>
2023-04-20 20:12:24 +0200

    Add basic lantern mechanics (#6)

---
+++ ribiz/Assets/LanternOil/LanternOil.gd
@@ -0,0 +1,18 @@
+class_name LanternOil
+extends Node2D
+
+signal lantern_oil_collected(amount)
+
+var Player = preload("res://Assets/Player/Scripts/Player.gd")
+export var amount: float = 20.0
+
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	add_to_group("lantern_oil")
+	animation_player.play("Bounce")
+
+func _on_body_entered(body):
+	if body is Player:
+		emit_signal("lantern_oil_collected", self.amount)
+		queue_free()
