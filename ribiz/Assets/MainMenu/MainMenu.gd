834a3473746b5fe75f31fdc7fee18e2b464a3d83
Lovro <lovro.bosnar1@gmail.com>
2023-04-21 23:26:01 +0200

    Add basic main menu (#12)

---
+++ ribiz/Assets/MainMenu/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/StartGameStoryButton.grab_focus()
+
+func _on_StartGameStoryButton_pressed():
+	get_tree().change_scene("res://Scenes/Game.tscn")
+
+func _on_ExitGameButton_pressed():
+	get_tree().quit()
+
+
+
