6809046ab352bb4d56d35f18c9d26c785e2ebac2
Borna Bešić <git.o8yim@aleeas.com>
2023-04-25 21:22:11 +0200

    Play the main theme

--- ribiz/Scenes/Game.gd
+++ ribiz/Scenes/Game.gd
@@ -4,10 +4,12 @@ const GAME_OVER_TIMEOUT: float = 3.0
 
 onready var game_over_hud = preload("res://Assets/HUD/GameOver.tscn").instance()
 onready var tree = get_tree()
+onready var theme = $ThemePlayer
 var is_game_over: bool = false
 
 func _ready():
 	$Player.connect("lantern_oil_changed", $LanternHealthHUD, "set_lantern_oil")
+	$Player.connect("lantern_oil_changed", self, "_set_theme_volume")
 	$Player.connect("lantern_extinguished", self, "_set_game_over")
 	for lantern_oil in tree.get_nodes_in_group("lantern_oil"):
 		lantern_oil.connect("lantern_oil_collected", $Player, "add_lantern_oil")
@@ -21,6 +23,10 @@ func _process(delta):
 		yield(tree.create_timer(GAME_OVER_TIMEOUT), "timeout")
 		tree.reload_current_scene()
 
+func _set_theme_volume(volume_percentage: float):
+	theme.volume_db = 10 * log(volume_percentage) / log(10)
+
 func _set_game_over():
 	self.add_child(game_over_hud)
+	theme.stop()
 	is_game_over = true
