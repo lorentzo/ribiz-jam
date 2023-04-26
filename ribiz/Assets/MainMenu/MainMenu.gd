7eb96de9ad229822c29e9e76daaeaf8021949fd7
Borna Bešić <bornabesic@users.noreply.github.com>
2023-04-26 03:38:15 +0200

    Enhance the main menu and add credits screen (#31)

--- ribiz/Assets/MainMenu/MainMenu.gd
+++ ribiz/Assets/MainMenu/MainMenu.gd
@@ -6,5 +6,8 @@ func _ready():
 func _on_StartGameStoryButton_pressed():
 	get_tree().change_scene("res://Scenes/Game.tscn")
 
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://Scenes/Credits.tscn")
+
 func _on_ExitGameButton_pressed():
 	get_tree().quit()
