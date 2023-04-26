5b31f195ec34f1f440d5a6b2bb0e8be6075eda7c
Lovro Bosnar <lovro.bosnar1@gmail.com>
2023-04-26 16:03:01 +0200

    update lore

--- ribiz/Assets/Lore/Lore.gd
+++ ribiz/Assets/Lore/Lore.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
-		get_tree().change_scene("res://Scenes/Game.tscn")
+			get_tree().change_scene("res://Scenes/Game.tscn")
