1540faaee3f5bf5500d8d6fcc9278536bf18cce0
Lovro Bosnar <lovro.bosnar1@gmail.com>
2023-04-26 15:42:45 +0200

    adding basic lore

--- ribiz/Assets/MainMenu/MainMenu.gd
+++ ribiz/Assets/MainMenu/MainMenu.gd
@@ -4,7 +4,8 @@ func _ready():
 	$VBoxContainer/StartGameStoryButton.grab_focus()
 
 func _on_StartGameStoryButton_pressed():
-	get_tree().change_scene("res://Scenes/Game.tscn")
+	#get_tree().change_scene("res://Scenes/Game.tscn")
+	get_tree().change_scene("res://Scenes/Lore.tscn")
 
 func _on_CreditsButton_pressed():
 	get_tree().change_scene("res://Scenes/Credits.tscn")
